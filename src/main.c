41295e081648dbaeec7e44bcff14ce7516693cc2
Iago Sousa <sousair2001@gmail.com>
2023-09-27 16:59:28 -0300

    feat: improve error handling

--- src/main.c
+++ src/main.c
@@ -2,29 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "../utils/bool.h"
+#include <../utils/bool.h>
 
 #define HACK_ASSEMBLY_EXTENSION ".asm"
 #define HACK_ASSEMBLY_EXTENSION_SIZE 4
+#define MAX_CHARACTERS_PER_LINE 512
+
+boolean check_file_extension(int arg_count, char *arg_values[]);
 
 int main(int argc, char *argv[])
 {
   if (check_file_extension(argc, argv))
   {
-    return 1;
+    return -1;
   }
 }
 
 boolean check_file_extension(int arg_count, char *arg_values[])
 {
+  if (arg_count != 2)
   {
+    // TODO: Add proper usage message.
+    fprintf(stderr, "[Error] Input file is mandatory\n");
     return false;
   }
 
   char *filename = arg_values[1];
-  if (strstr(arg_values[1], HACK_ASSEMBLY_EXTENSION))
+  if (strstr(arg_values[1], HACK_ASSEMBLY_EXTENSION) == NULL)
   {
-    printf("Input should have the extension %s. Received %s", HACK_ASSEMBLY_EXTENSION, filename);
+    fprintf(stderr, "[Error] Input should have the extension %s. Received %s\n", HACK_ASSEMBLY_EXTENSION, filename); 
     return false;
   }
 
