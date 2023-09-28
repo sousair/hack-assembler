72aa84c524c03fb581f848be13f7b5a7b6ceb186
Iago Sousa <sousair2001@gmail.com>
2023-09-28 09:20:21 -0300

    feat: initial symbols setup

--- src/main.c
+++ src/main.c
@@ -2,20 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include <../utils/bool.h>
+#include <symbol.h>
+#include <bool.h>
 
 #define HACK_ASSEMBLY_EXTENSION ".asm"
 #define HACK_ASSEMBLY_EXTENSION_SIZE 4
-#define MAX_CHARACTERS_PER_LINE 512
 
 boolean check_file_extension(int arg_count, char *arg_values[]);
+void increment_symbol_address(int *address);
+void add_default_symbols(SYMBOL hash_table[SYMBOL_HASH_TABLE_MAX_SIZE]);
 
 int main(int argc, char *argv[])
 {
-  if (check_file_extension(argc, argv))
+  if (!check_file_extension(argc, argv))
   {
-    return -1;
+    return 1;
   }
+
+  SYMBOL hash_table[SYMBOL_HASH_TABLE_MAX_SIZE];
+
+  add_default_symbols(hash_table);
+  return 0;
 }
 
 boolean check_file_extension(int arg_count, char *arg_values[])
@@ -30,9 +37,55 @@ boolean check_file_extension(int arg_count, char *arg_values[])
   char *filename = arg_values[1];
   if (strstr(arg_values[1], HACK_ASSEMBLY_EXTENSION) == NULL)
   {
-    fprintf(stderr, "[Error] Input should have the extension %s. Received %s\n", HACK_ASSEMBLY_EXTENSION, filename); 
+    fprintf(stderr, "[Error] Input should have the extension %s. Received %s\n", HACK_ASSEMBLY_EXTENSION, filename);
     return false;
   }
 
   return true;
+}
+
+void increment_symbol_address(int *address)
+{
+  int reserved_addresses[2] = {16384, 24576};
+
+  (*address)++;
+
+  if (*address == reserved_addresses[0] || *address == reserved_addresses[1])
+  {
+    (*address)++;
+  }
+
+  if (*address > 0x7FFF)
+  {
+    fprintf(stderr, "[Error] Max address exceeded\n");
+    *address = -1;
+  }
+}
+
+void add_default_symbols(SYMBOL hash_table[SYMBOL_HASH_TABLE_MAX_SIZE])
+{
+  add_symbol("R0", 0, hash_table);
+  add_symbol("R1", 1, hash_table);
+  add_symbol("R2", 2, hash_table);
+  add_symbol("R3", 3, hash_table);
+  add_symbol("R4", 4, hash_table);
+  add_symbol("R5", 5, hash_table);
+  add_symbol("R6", 6, hash_table);
+  add_symbol("R7", 7, hash_table);
+  add_symbol("R8", 8, hash_table);
+  add_symbol("R9", 9, hash_table);
+  add_symbol("R10", 10, hash_table);
+  add_symbol("R11", 11, hash_table);
+  add_symbol("R12", 12, hash_table);
+  add_symbol("R13", 13, hash_table);
+  add_symbol("R14", 14, hash_table);
+  add_symbol("R15", 15, hash_table);
+  add_symbol("SP", 0, hash_table);
+  add_symbol("LCL", 1, hash_table);
+  add_symbol("ARG", 2, hash_table);
+  add_symbol("THIS", 3, hash_table);
+  add_symbol("THAT", 4, hash_table);
+  add_symbol("SCREEN", 16384, hash_table);
+  add_symbol("KBD", 24576, hash_table);
+  add_symbol("LOOP", 4, hash_table);
 }
